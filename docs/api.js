22662e51a1712fc5798aa92fc4710a0664c59daa
xeolabs <lindsay.kay@xeolabs.com>
2015-08-13 18:08:44 +0200

    Docs

--- docs/api.js
+++ docs/api.js
@@ -2,7 +2,10 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "AmbientLight",
+        "Boundary3D",
         "Camera",
+        "CameraControl",
+        "CameraFlyAnimation",
         "Canvas",
         "Clip",
         "Clips",
@@ -17,13 +20,23 @@ YUI.add("yuidoc-meta", function(Y) {
         "Frustum",
         "GameObject",
         "Geometry",
+        "Group",
+        "GroupBoundary",
         "Input",
+        "KeyboardAxisCamera",
+        "KeyboardOrbitCamera",
+        "KeyboardPanCamera",
+        "KeyboardZoomCamera",
         "Layer",
         "Lights",
         "Lookat",
         "Material",
         "Modes",
         "MorphTargets",
+        "MouseOrbitCamera",
+        "MousePanCamera",
+        "MousePickObject",
+        "MouseZoomCamera",
         "Ortho",
         "PBRMaterial",
         "Perspective",
@@ -48,9 +61,117 @@ YUI.add("yuidoc-meta", function(Y) {
         "math"
     ],
     "modules": [
-        "XEO"
+        "XEO",
+        "animation",
+        "clipping",
+        "configuration",
+        "culling",
+        "geometry",
+        "grouping",
+        "input",
+        "lighting",
+        "materials",
+        "math",
+        "objects",
+        "ordering",
+        "rendering",
+        "reporting",
+        "rtt",
+        "shading",
+        "spatial",
+        "transforms"
     ],
     "allModules": [
+        {
+            "displayName": "animation",
+            "name": "animation",
+            "description": "Components for animating state within Scenes."
+        },
+        {
+            "displayName": "clipping",
+            "name": "clipping",
+            "description": "Components for cross-section views of GameObjects."
+        },
+        {
+            "displayName": "configuration",
+            "name": "configuration",
+            "description": "Components for managing Scene configuration."
+        },
+        {
+            "displayName": "culling",
+            "name": "culling",
+            "description": "Components for controlling the visibility of GameObjects."
+        },
+        {
+            "displayName": "geometry",
+            "name": "geometry",
+            "description": "Components for defining geometry."
+        },
+        {
+            "displayName": "grouping",
+            "name": "grouping",
+            "description": "Components for managing groups of components."
+        },
+        {
+            "displayName": "input",
+            "name": "input",
+            "description": "Components for capturing user input."
+        },
+        {
+            "displayName": "lighting",
+            "name": "lighting",
+            "description": "Components for defining light sources."
+        },
+        {
+            "displayName": "materials",
+            "name": "materials",
+            "description": "Components to define the surface appearance of GameObjects."
+        },
+        {
+            "displayName": "math",
+            "name": "math",
+            "description": "Math utilities."
+        },
+        {
+            "displayName": "objects",
+            "name": "objects",
+            "description": "Game object components."
+        },
+        {
+            "displayName": "ordering",
+            "name": "ordering",
+            "description": "Components for controlling the rendering order of GameObjects."
+        },
+        {
+            "displayName": "rendering",
+            "name": "rendering",
+            "description": "Components that influence the way objects are rendered on WebGL."
+        },
+        {
+            "displayName": "reporting",
+            "name": "reporting",
+            "description": "Components for reporting Scene statistics."
+        },
+        {
+            "displayName": "rtt",
+            "name": "rtt",
+            "description": "Components to support render-to-texture (RTT) effects."
+        },
+        {
+            "displayName": "shading",
+            "name": "shading",
+            "description": "Components for custom shading."
+        },
+        {
+            "displayName": "spatial",
+            "name": "spatial",
+            "description": "Components to support spatial queries (eg. collisions etc)."
+        },
+        {
+            "displayName": "transforms",
+            "name": "transforms",
+            "description": "Components for Modelling, View and Projection transformation."
+        },
         {
             "displayName": "XEO",
             "name": "XEO",
