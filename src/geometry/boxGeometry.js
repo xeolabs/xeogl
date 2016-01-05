6d043f5a9d5d217d4670965d991c6da8bd168ec4
xeolabs <lindsay.kay@xeolabs.com>
2016-01-05 11:03:29 +0100

    Fixed duplicate indices member in BoxGeometry https://github.com/xeolabs/xeoengine/issues/27

--- src/geometry/boxGeometry.js
+++ src/geometry/boxGeometry.js
@@ -214,15 +214,6 @@
             // for Normal mapping once we know we have texture
 
             this.tangents = null;
-
-            this.indices = [
-                0, 1, 2, 0, 2, 3,    // front
-                4, 5, 6, 4, 6, 7,    // back
-                8, 9, 10, 8, 10, 11,   // top
-                12, 13, 14, 12, 14, 15,   // bottom
-                16, 17, 18, 16, 18, 19,   // right
-                20, 21, 22, 20, 22, 23    // left
-            ];
         },
 
         _props: {
