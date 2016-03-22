b1732b672b1b05b429f57d4d3bfc464c3fc12d15
xeolabs <lindsay.kay@xeolabs.com>
2016-03-22 09:36:17 +0100

    Show spinner for texture loads

--- build/xeoengine.js
+++ build/xeoengine.js
@@ -15301,6 +15301,8 @@ XEO.math.b3 = function (t, p0, p1, p2, p3) {
  <ul>
  <li>Spinners are normally shown by {{#crossLink "Model"}}Models{{/crossLink}} while they are loading, however they may also
  be shown by any application code that wants to indicate business.</li>
+ <li>By default, they are also shown by components that load assets, such as {{#crossLink "Texture"}}{{/crossLink}}. You
+ can disable that by flipping the Spinner's {{#crossLink "Spinner/textures:property"}}{{/crossLink}} property.</li>
  <li>A Spinner component has a {{#crossLink "Spinner/processes:property"}}{{/crossLink}} count that indicates how many
  active processes it currently represents. As a process starts, a process would increment {{#crossLink "Spinner/processes:property"}}{{/crossLink}}, then as it
  completes (or fails), would decrement it again.</li>
@@ -15337,6 +15339,14 @@ XEO.math.b3 = function (t, p0, p1, p2, p3) {
  // so spinner becomes invisible
  spinner.process--;
 ````
+
+ By default, a Spinner shows while resources are loading for components like
+ {{#crossLink "Texture"}}{{/crossLink}}. We can disable that like this:
+
+ ````javascript
+ // Don't show while resources are loading for Textures etc.
+ spinner.textures = false;
+ ````
 
  @class Spinner
  @module XEO
@@ -15392,9 +15402,42 @@ XEO.math.b3 = function (t, p0, p1, p2, p3) {
             this._adjustPosition();
 
             this.processes = 0;
+
+            this.textures = cfg.textures;
         },
 
         _props: {
+
+            /**
+             * Whether Spinner shows while images are loading for components like {{#crossLink "Texture"}}{{/crossLink}}.
+             *
+             * Fires a {{#crossLink "Spinner/textures:event"}}{{/crossLink}} event on change.
+             *
+             * @property textures
+             * @default true
+             * @type Boolean
+             */
+            textures: {
+
+                set: function (value) {
+
+                    value = value !== false;
+
+                    this._textures = value;
+                    
+                    /**
+                     * Fired whenever this Spinner's  {{#crossLink "Spinner/textures:property"}}{{/crossLink}} property changes.
+                     *
+                     * @event textures
+                     * @param value The property's new value
+                     */
+                    this.fire("textures", this._textures);
+                },
+
+                get: function () {
+                    return this._textures;
+                }
+            },
 
             /**
              The number of processes this Spinner represents.
@@ -30972,6 +31015,9 @@ XEO.GLTFLoaderUtils = Object.create(Object, {
 
             var image = new Image();
 
+            var spinner = this.scene.canvas.spinner;
+            var spinnerTextures = spinner.textures;
+
             image.onload = function () {
 
                 if (self._src === src) {
@@ -30986,6 +31032,10 @@ XEO.GLTFLoaderUtils = Object.create(Object, {
                     self._imageDirty = true;
                     self._srcDirty = false;
                     self._targetDirty = false;
+
+                    if (spinnerTextures) {
+                        spinner.processes--;
+                    }
 
                     self._scheduleUpdate();
 
@@ -31010,6 +31060,9 @@ XEO.GLTFLoaderUtils = Object.create(Object, {
 
             image.onerror = function () {
                 //              task.setFailed();
+                if (spinnerTextures) {
+                    spinner.processes--;
+                }
             };
 
             if (src.indexOf("data") === 0) {
@@ -31022,6 +31075,10 @@ XEO.GLTFLoaderUtils = Object.create(Object, {
                 // Image file
                 image.crossOrigin = "Anonymous";
                 image.src = src;
+
+                if (spinnerTextures) {
+                    spinner.processes++;
+                }
             }
         },
 
