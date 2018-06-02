43eddc91faeda537a33b76c810da7e7c8b78a54e
xeolabs <lindsay.kay@xeolabs.com>
2018-06-02 14:46:57 +0200

    Geometry tidyups

--- src/geometry/geometry.js
+++ src/geometry/geometry.js
@@ -1236,85 +1236,85 @@
         return array[i] * vec3[0] + array[i + 1] * vec3[1] + array[i + 2] * vec3[2];
     }
 
-    /*
-     * Checks for duplicate vertices with hashmap.
-     * Duplicated vertices are removed
-     * and faces' vertices are updated.
-     */
-
-    // Should return { positions:  newPositions, indices: newIndices };
-    var mergeVertices = function (positions, indices) {
-
-        var verticesMap = {}; // Hashmap for looking up vertices by position coordinates (and making sure they are unique)
-        var unique = [], changes = [];
-
-        var v, key;
-        var precisionPoints = 4; // number of decimal points, e.g. 4 for epsilon of 0.0001
-        var precision = Math.pow(10, precisionPoints);
-        var i, il, face;
-        var indices, j, jl;
-
-        for (i = 0, il = this.vertices.length; i < il; i++) {
-
-            v = this.vertices[i];
-            key = Math.round(v.x * precision) + '_' + Math.round(v.y * precision) + '_' + Math.round(v.z * precision);
-
-            if (verticesMap[key] === undefined) {
-
-                verticesMap[key] = i;
-                unique.push(this.vertices[i]);
-                changes[i] = unique.length - 1;
-
-            } else {
-
-                //console.log('Duplicate vertex found. ', i, ' could be using ', verticesMap[key]);
-                changes[i] = changes[verticesMap[key]];
-            }
-        }
-
-        // if faces are completely degenerate after merging vertices, we
-        // have to remove them from the geometry.
-        var faceIndicesToRemove = [];
-
-        for (i = 0, il = this.faces.length; i < il; i++) {
-
-            face = this.faces[i];
-
-            face.a = changes[face.a];
-            face.b = changes[face.b];
-            face.c = changes[face.c];
-
-            indices = [face.a, face.b, face.c];
-
-            // if any duplicate vertices are found in a Face3
-            // we have to remove the face as nothing can be saved
-            for (var n = 0; n < 3; n++) {
-                if (indices[n] === indices[( n + 1 ) % 3]) {
-                    faceIndicesToRemove.push(i);
-                    break;
-                }
-            }
-        }
-
-        for (i = faceIndicesToRemove.length - 1; i >= 0; i--) {
-            var idx = faceIndicesToRemove[i];
-            this.faces.splice(idx, 1);
-            for (j = 0, jl = this.faceVertexUvs.length; j < jl; j++) {
-                this.faceVertexUvs[j].splice(idx, 1);
-            }
-        }
-
-        // Use unique set of vertices
-
-        var diff = this.vertices.length - unique.length;
-        this.vertices = unique;
-        return diff;
-    };
-
 
     var buildEdgesIndices = (function () {
 
         var math = xeogl.math;
+
+        /*
+         * Checks for duplicate vertices with hashmap.
+         * Duplicated vertices are removed
+         * and faces' vertices are updated.
+         */
+
+        // // Should return { positions:  newPositions, indices: newIndices };
+        // var mergeVertices = function (positions, indices) {
+        //
+        //     var verticesMap = {}; // Hashmap for looking up vertices by position coordinates (and making sure they are unique)
+        //     var unique = [], changes = [];
+        //
+        //     var v, key;
+        //     var precisionPoints = 4; // number of decimal points, e.g. 4 for epsilon of 0.0001
+        //     var precision = Math.pow(10, precisionPoints);
+        //     var i, il, face;
+        //     var indices, j, jl;
+        //
+        //     for (i = 0, il = this.vertices.length; i < il; i++) {
+        //
+        //         v = this.vertices[i];
+        //         key = Math.round(v.x * precision) + '_' + Math.round(v.y * precision) + '_' + Math.round(v.z * precision);
+        //
+        //         if (verticesMap[key] === undefined) {
+        //
+        //             verticesMap[key] = i;
+        //             unique.push(this.vertices[i]);
+        //             changes[i] = unique.length - 1;
+        //
+        //         } else {
+        //
+        //             //console.log('Duplicate vertex found. ', i, ' could be using ', verticesMap[key]);
+        //             changes[i] = changes[verticesMap[key]];
+        //         }
+        //     }
+        //
+        //     // if faces are completely degenerate after merging vertices, we
+        //     // have to remove them from the geometry.
+        //     var faceIndicesToRemove = [];
+        //
+        //     for (i = 0, il = this.faces.length; i < il; i++) {
+        //
+        //         face = this.faces[i];
+        //
+        //         face.a = changes[face.a];
+        //         face.b = changes[face.b];
+        //         face.c = changes[face.c];
+        //
+        //         indices = [face.a, face.b, face.c];
+        //
+        //         // if any duplicate vertices are found in a Face3
+        //         // we have to remove the face as nothing can be saved
+        //         for (var n = 0; n < 3; n++) {
+        //             if (indices[n] === indices[( n + 1 ) % 3]) {
+        //                 faceIndicesToRemove.push(i);
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //
+        //     for (i = faceIndicesToRemove.length - 1; i >= 0; i--) {
+        //         var idx = faceIndicesToRemove[i];
+        //         this.faces.splice(idx, 1);
+        //         for (j = 0, jl = this.faceVertexUvs.length; j < jl; j++) {
+        //             this.faceVertexUvs[j].splice(idx, 1);
+        //         }
+        //     }
+        //
+        //     // Use unique set of vertices
+        //
+        //     var diff = this.vertices.length - unique.length;
+        //     this.vertices = unique;
+        //     return diff;
+        // };
 
         var faces = [];
         var numFaces = 0;
