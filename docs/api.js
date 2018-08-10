89a19fe864b23e13873fd0e4031fcdef5aea5551
xeolabs <lindsay.kay@xeolabs.com>
2018-08-10 13:53:17 +0200

    Snapshot build

---
+++ docs/api.js
@@ -0,0 +1,224 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AABBGeometry",
+        "AABBHelper",
+        "AmbientLight",
+        "Annotation",
+        "AnnotationStory",
+        "AxisHelper",
+        "BIMServerModel",
+        "BoxGeometry",
+        "BuildableModel",
+        "Button",
+        "Camera",
+        "CameraControl",
+        "CameraFlightAnimation",
+        "CameraFollowAnimation",
+        "CameraPath",
+        "CameraPathAnimation",
+        "Canvas",
+        "Clip",
+        "ClipControl",
+        "ClipHelper",
+        "Component",
+        "CubeTexture",
+        "CubicBezierCurve",
+        "Curve",
+        "CustomProjection",
+        "CylinderGeometry",
+        "DirLight",
+        "EdgeMaterial",
+        "EmphasisMaterial",
+        "Fresnel",
+        "Frustum",
+        "GLTFModel",
+        "Geometry",
+        "GeometryBuilder",
+        "Group",
+        "HeightmapGeometry",
+        "Input",
+        "LabelHelper",
+        "LambertMaterial",
+        "LightMap",
+        "Material",
+        "Mesh",
+        "MetallicMaterial",
+        "Model",
+        "OBBGeometry",
+        "OBBHelper",
+        "OBJModel",
+        "Object",
+        "Ortho",
+        "OutlineMaterial",
+        "Path",
+        "PathGeometry",
+        "Perspective",
+        "PhongMaterial",
+        "Pin",
+        "PlaneGeometry",
+        "PlaneHelper",
+        "PointLight",
+        "QuadraticBezierCurve",
+        "ReflectionMap",
+        "STLModel",
+        "Scene",
+        "SceneJSModel",
+        "Shadow",
+        "Skybox",
+        "SpecularMaterial",
+        "SphereGeometry",
+        "Spinner",
+        "SplineCurve",
+        "SplineCurveHelper",
+        "SpotLight",
+        "StereoEffect",
+        "Story",
+        "TeapotGeometry",
+        "TestModel",
+        "Texture",
+        "TorusGeometry",
+        "VectorTextGeometry",
+        "Viewport",
+        "XML3DModel",
+        "xeogl",
+        "xeogl.math.math"
+    ],
+    "modules": [
+        "animation",
+        "annotations",
+        "camera",
+        "canvas",
+        "clipping",
+        "controls",
+        "curves",
+        "effects",
+        "generation",
+        "geometry",
+        "helpers",
+        "input",
+        "lighting",
+        "materials",
+        "math",
+        "meshes",
+        "models",
+        "objects",
+        "rendering",
+        "skyboxes",
+        "stories",
+        "xeogl"
+    ],
+    "allModules": [
+        {
+            "displayName": "animation",
+            "name": "animation",
+            "description": "Components for animating state within Scenes."
+        },
+        {
+            "displayName": "annotations",
+            "name": "annotations",
+            "description": "Components for annotating meshes."
+        },
+        {
+            "displayName": "camera",
+            "name": "camera",
+            "description": "Camera components."
+        },
+        {
+            "displayName": "canvas",
+            "name": "canvas",
+            "description": "Canvas-related things."
+        },
+        {
+            "displayName": "clipping",
+            "name": "clipping",
+            "description": "Components for cross-section views of meshes."
+        },
+        {
+            "displayName": "controls",
+            "name": "controls",
+            "description": "Components for controlling things with user input."
+        },
+        {
+            "displayName": "curves",
+            "name": "curves",
+            "description": "Components for defining 3D curves."
+        },
+        {
+            "displayName": "effects",
+            "name": "effects",
+            "description": "Components for viewing effects."
+        },
+        {
+            "displayName": "generation",
+            "name": "generation",
+            "description": "Components for generating xeogl content."
+        },
+        {
+            "displayName": "geometry",
+            "name": "geometry",
+            "description": "Components for defining geometry."
+        },
+        {
+            "displayName": "helpers",
+            "name": "helpers",
+            "description": "Components to help visualise other components."
+        },
+        {
+            "displayName": "input",
+            "name": "input",
+            "description": "Components for capturing user input."
+        },
+        {
+            "displayName": "lighting",
+            "name": "lighting",
+            "description": "Components for defining light sources."
+        },
+        {
+            "displayName": "materials",
+            "name": "materials",
+            "description": "Components to define the surface appearance of Meshes."
+        },
+        {
+            "displayName": "math",
+            "name": "math",
+            "description": "Math utilities."
+        },
+        {
+            "displayName": "meshes",
+            "name": "meshes",
+            "description": "Shows the shape and control points of {{#crossLink \"SplineCurve\"}}{{/crossLink}}"
+        },
+        {
+            "displayName": "models",
+            "name": "models",
+            "description": "Models are units of xeogl content."
+        },
+        {
+            "displayName": "objects",
+            "name": "objects",
+            "description": "3D elements within xeogl Scenes."
+        },
+        {
+            "displayName": "rendering",
+            "name": "rendering",
+            "description": "Components that influence the way meshes are rendered with WebGL."
+        },
+        {
+            "displayName": "skyboxes",
+            "name": "skyboxes",
+            "description": "Skybox components."
+        },
+        {
+            "displayName": "stories",
+            "name": "stories",
+            "description": "UI presentation components."
+        },
+        {
+            "displayName": "xeogl",
+            "name": "xeogl",
+            "description": "The xeogl namespace."
+        }
+    ]
+} };
+});
