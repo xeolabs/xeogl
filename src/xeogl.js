578b491be7a05b69b23e7be93f631b099bbdad8f
xeolabs <lindsay.kay@xeolabs.com>
2018-08-31 12:47:29 +0200

    Finalized rollup input file

--- src/xeogl.js
+++ src/xeogl.js
@@ -7,98 +7,64 @@
  @author xeolabs / http://xeolabs.com/
  */
 
-
 import {core} from "./core.js";
-export {WEBGL_INFO}  from "./webglInfo.js";
+import {tasks} from "./tasks.js";
+import {loop} from "./loop.js";
+
+// Core framework
+
+export {WEBGL_INFO} from "./webglInfo.js";
 export {stats}  from "./stats.js";
 export {math} from "./math/math.js";
-// export {Component} from "./component.js";
-// export {CameraFlightAnimation} from './animation/cameraFlightAnimation.js';
-// export {Canvas} from "./canvas/canvas.js";
-// export {Spinner} from "./canvas/spinner.js";
-// export {Clip} from "./clipping/clip.js";
-// export {CameraControl} from "./controls/cameraControl.js";
-// export {Geometry} from "./geometry/geometry.js";
-// export {BoxGeometry} from "./geometry/boxGeometry.js";
-// export {TorusGeometry} from "./geometry/torusGeometry.js";
-// export {SphereGeometry} from "./geometry/sphereGeometry.js";
-// export {OBBGeometry} from "./geometry/obbGeometry.js";
-// export {AABBGeometry}  from "./geometry/aabbGeometry.js";
-// // export {PathGeometry} from "./geometry/pathGeometry.js";
-// export {CylinderGeometry} from "./geometry/cylinderGeometry.js";
-// export {PlaneGeometry} from "./geometry/planeGeometry.js";
-// export {Input} from "./input/input.js";
-// export {AmbientLight} from "./lighting/ambientLight.js";
-// export {DirLight} from "./lighting/dirLight.js";
-// export {PointLight} from "./lighting/pointLight.js";
-// export {SpotLight} from "./lighting/spotLight.js";
-// export {CubeTexture} from "./lighting/cubeTexture.js";
-// export {LightMap} from "./lighting/lightMap.js";
-// export {ReflectionMap} from "./lighting/reflectionMap.js";
-// export {Shadow} from "./lighting/shadow.js";
-// export {Model} from "./models/model.js";
-// export {Material} from "./materials/material.js";
-// export {PhongMaterial} from "./materials/phongMaterial.js";
-// export {LambertMaterial} from "./materials/lambertMaterial.js";
-// export {SpecularMaterial} from "./materials/specularMaterial.js";
-// export {MetallicMaterial} from "./materials/metallicMaterial.js";
-// export {EmphasisMaterial} from "./materials/emphasisMaterial.js";
-// export {EdgeMaterial} from "./materials/edgeMaterial.js";
-// export {OutlineMaterial} from "./materials/outlineMaterial.js";
-// export {Texture} from "./materials/texture.js";
-// export {Fresnel} from "./materials/fresnel.js";
-// export {Viewport} from "./viewport/viewport.js";
-// export {Camera} from "./camera/camera.js";
-// export {Frustum} from "./camera/frustum.js";
-// export {Ortho} from "./camera/ortho.js";
-// export {Perspective} from "./camera/perspective.js";
-// export {CustomProjection} from "./camera/customProjection.js"
-// export {Scene} from "./scene.js";
 
-//
-// xeogl.WEBGL_INFO = WEBGL_INFO;
-// xeogl.stats = stats;
-// xeogl.math = math;
-// xeogl.Component = Component;
-// xeogl.CameraFlightAnimation = CameraFlightAnimation;
-// // xeogl.Canvas = Canvas;
-// // xeogl.Spinner = Spinner;
-// // xeogl.Clip = Clip;
-// // xeogl.CameraControl = CameraControl;
-// // xeogl.Geometry = Geometry;
-// // xeogl.BoxGeometry = BoxGeometry;
-// // xeogl.TorusGeometry = TorusGeometry;
-// // xeogl.SphereGeometry = SphereGeometry;
-// // xeogl.OBBGeometry = OBBGeometry;
-// // xeogl.AABBGeometry = AABBGeometry;
-// // xeogl.CylinderGeometry = CylinderGeometry;
-// // xeogl.PlaneGeometry = PlaneGeometry;
-// // xeogl.Input = Input;
-// // xeogl.AmbientLight = AmbientLight;
-// // xeogl.DirLight = DirLight;
-// // xeogl.PointLight = PointLight;
-// // xeogl.SpotLight = SpotLight;
-// // xeogl.CubeTexture = CubeTexture;
-// // xeogl.LightMap = LightMap;
-// // xeogl.ReflectionMap = ReflectionMap;
-// // xeogl.Shadow = Shadow;
-// // xeogl.Model = Model;
-// // xeogl.Material = Material;
-// // xeogl.PhongMaterial = PhongMaterial;
-// // xeogl.MetallicMaterial = MetallicMaterial;
-// // xeogl.SpecularMaterial = SpecularMaterial;
-// // xeogl.LambertMaterial = LambertMaterial;
-// // xeogl.EmphasisMaterial = EmphasisMaterial;
-// // xeogl.EdgeMaterial =  EdgeMaterial;
-// // xeogl.OutlineMaterial = OutlineMaterial;
-// // xeogl.Texture = Texture;
-// // xeogl.Fresnel = Fresnel;
-// // xeogl.Viewport = Viewport;
-// // xeogl.Camera = Camera;
-// // xeogl.Frustum = Frustum;
-// // xeogl.Ortho = Ortho;
-// // xeogl.Perspective = Perspective;
-// // xeogl.CustomProjection = CustomProjection;
-// // xeogl.Scene = Scene;
-//
-// export default xeogl;
+export const scheduleTask = tasks.scheduleTask;
+export const clear = core.clear;
+
+// Component classes
+
+export {Component} from "./component.js";
+export {CameraFlightAnimation} from './animation/cameraFlightAnimation.js';
+export {Canvas} from "./canvas/canvas.js";
+export {Spinner} from "./canvas/spinner.js";
+export {Clip} from "./clipping/clip.js";
+export {CameraControl} from "./controls/cameraControl.js";
+export {Geometry} from "./geometry/geometry.js";
+export {BoxGeometry} from "./geometry/boxGeometry.js";
+export {TorusGeometry} from "./geometry/torusGeometry.js";
+export {SphereGeometry} from "./geometry/sphereGeometry.js";
+export {OBBGeometry} from "./geometry/obbGeometry.js";
+export {AABBGeometry}  from "./geometry/aabbGeometry.js";
+export {CylinderGeometry} from "./geometry/cylinderGeometry.js";
+export {PlaneGeometry} from "./geometry/planeGeometry.js";
+export {Input} from "./input/input.js";
+export {AmbientLight} from "./lighting/ambientLight.js";
+export {DirLight} from "./lighting/dirLight.js";
+export {PointLight} from "./lighting/pointLight.js";
+export {SpotLight} from "./lighting/spotLight.js";
+export {CubeTexture} from "./lighting/cubeTexture.js";
+export {LightMap} from "./lighting/lightMap.js";
+export {ReflectionMap} from "./lighting/reflectionMap.js";
+export {Shadow} from "./lighting/shadow.js";
+export {Model} from "./models/model.js";
+export {Mesh} from "./objects/mesh.js";
+export {Group} from "./objects/group.js";
+export {xeoglObject as Object} from "./objects/object.js";
+export {Material} from "./materials/material.js";
+export {PhongMaterial} from "./materials/phongMaterial.js";
+export {LambertMaterial} from "./materials/lambertMaterial.js";
+export {SpecularMaterial} from "./materials/specularMaterial.js";
+export {MetallicMaterial} from "./materials/metallicMaterial.js";
+export {EmphasisMaterial} from "./materials/emphasisMaterial.js";
+export {EdgeMaterial} from "./materials/edgeMaterial.js";
+export {OutlineMaterial} from "./materials/outlineMaterial.js";
+export {Texture} from "./materials/texture.js";
+export {Fresnel} from "./materials/fresnel.js";
+export {Viewport} from "./viewport/viewport.js";
+export {Camera} from "./camera/camera.js";
+export {Frustum} from "./camera/frustum.js";
+export {Ortho} from "./camera/ortho.js";
+export {Perspective} from "./camera/perspective.js";
+export {CustomProjection} from "./camera/customProjection.js"
+export {Scene} from "./scene.js";
+
+
