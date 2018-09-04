e3d8cb082407fd35959de31fd66e55eb40f14ba2
xeolabs <lindsay.kay@xeolabs.com>
2018-09-04 12:19:13 +0200

    Convert curves components to EC6

--- examples/js/curves/splineCurve.js
+++ examples/js/curves/splineCurve.js
@@ -102,149 +102,128 @@
  @param [cfg.t=0] Current position on this SplineCurve, in range between 0..1.
  @extends Curve
  */
-(function () {
+xeogl.SplineCurve = class xeoglSplineCurve extends xeogl.Curve {
 
-    "use strict";
+    /**
+     JavaScript class name for this Component.
 
-    xeogl.SplineCurve = xeogl.Curve.extend({
+     For example: "xeogl.AmbientLight", "xeogl.ColorTarget", "xeogl.Lights" etc.
 
+     @property type
+     @type String
+     @final
+     */
+    get type() {
+        return "xeogl.SplineCurve";
+    }
+
+    init(cfg) {
+        super.init(cfg);
+        this.points = cfg.points;
+        this.t = cfg.t;
+    }
+
+    /**
+     Control points on this SplineCurve.
+
+     Fires a {{#crossLink "SplineCurve/points:event"}}{{/crossLink}} event on change.
+
+     @property points
+     @default []
+     @type Float32Array
+     */
+    set points(value) {
+        this._points = value || [];
         /**
-         JavaScript class name for this Component.
-
-         @property type
-         @type String
-         @final
+         * Fired whenever this SplineCurve's
+         * {{#crossLink "SplineCurve/points:property"}}{{/crossLink}} property changes.
+         * @event points
+         * @param value The property's new value
          */
-        type: "xeogl.SplineCurve",
+        this.fire("points", this._points);
+    }
 
-        _init: function (cfg) {
+    get points() {
+        return this._points;
+    }
 
-            this._super(cfg);
+    /**
+     Progress along this SplineCurve.
 
-            this.points = cfg.points;
+     Automatically clamps to range [0..1].
 
-            this.t = cfg.t;
-        },
+     Fires a {{#crossLink "SplineCurve/t:event"}}{{/crossLink}} event on change.
 
-        _props: {
-
-            /**
-             Control points on this SplineCurve.
-
-             Fires a {{#crossLink "SplineCurve/points:event"}}{{/crossLink}} event on change.
-
-             @property points
-             @default []
-             @type Float32Array
-             */
-            points: {
-
-                set: function (value) {
-
-                    this._points = value || [];
-
-                    /**
-                     * Fired whenever this SplineCurve's
-                     * {{#crossLink "SplineCurve/points:property"}}{{/crossLink}} property changes.
-                     * @event points
-                     * @param value The property's new value
-                     */
-                    this.fire("points", this._points);
-                },
-
-                get: function () {
-                    return this._points;
-                }
-            },
-
-            /**
-             Progress along this SplineCurve.
-
-             Automatically clamps to range [0..1].
-
-             Fires a {{#crossLink "SplineCurve/t:event"}}{{/crossLink}} event on change.
-
-             @property t
-             @default 0
-             @type Number
-             */
-            t: {
-                set: function (value) {
-
-                    value = value || 0;
-
-                    this._t = value < 0.0 ? 0.0 : (value > 1.0 ? 1.0 : value);
-
-                    /**
-                     * Fired whenever this SplineCurve's
-                     * {{#crossLink "SplineCurve/t:property"}}{{/crossLink}} property changes.
-                     * @event t
-                     * @param value The property's new value
-                     */
-                    this.fire("t", this._t);
-                },
-
-                get: function () {
-                    return this._t;
-                }
-            },
-
-            /**
-             Point on this SplineCurve at position {{#crossLink "SplineCurve/t:property"}}{{/crossLink}}.
-
-             @property point
-             @type {{Array of Number}}
-             */
-            point: {
-
-                get: function () {
-                    return this.getPoint(this._t);
-                }
-            }
-        },
-
+     @property t
+     @default 0
+     @type Number
+     */
+    set t(value) {
+        value = value || 0;
+        this._t = value < 0.0 ? 0.0 : (value > 1.0 ? 1.0 : value);
         /**
-         * Returns point on this SplineCurve at the given position.
-         * @method getPoint
-         * @param {Number} t Position to get point at.
-         * @returns {{Array of Number}}
+         * Fired whenever this SplineCurve's
+         * {{#crossLink "SplineCurve/t:property"}}{{/crossLink}} property changes.
+         * @event t
+         * @param value The property's new value
          */
-        getPoint: function (t) {
+        this.fire("t", this._t);
+    }
 
-            var math = xeogl.math;
+    get t() {
+        return this._t;
+    }
 
-            var points = this.points;
+    /**
+     Point on this SplineCurve at position {{#crossLink "SplineCurve/t:property"}}{{/crossLink}}.
 
-            if (points.length < 3) {
-                this.error("Can't sample point from SplineCurve - not enough points on curve - returning [0,0,0].");
-                return;
-            }
+     @property point
+     @type {{Array of Number}}
+     */
+    get point() {
+        return this.getPoint(this._t);
+    }
 
-            var point = ( points.length - 1 ) * t;
+    /**
+     * Returns point on this SplineCurve at the given position.
+     * @method getPoint
+     * @param {Number} t Position to get point at.
+     * @returns {{Array of Number}}
+     */
+    getPoint(t) {
 
-            var intPoint = Math.floor(point);
-            var weight = point - intPoint;
+        var math = xeogl.math;
 
-            var point0 = points[intPoint === 0 ? intPoint : intPoint - 1];
-            var point1 = points[intPoint];
-            var point2 = points[intPoint > points.length - 2 ? points.length - 1 : intPoint + 1];
-            var point3 = points[intPoint > points.length - 3 ? points.length - 1 : intPoint + 2];
+        var points = this.points;
 
-            var vector = math.vec3();
-
-            vector[0] = math.catmullRomInterpolate(point0[0], point1[0], point2[0], point3[0], weight);
-            vector[1] = math.catmullRomInterpolate(point0[1], point1[1], point2[1], point3[1], weight);
-            vector[2] = math.catmullRomInterpolate(point0[2], point1[2], point2[2], point3[2], weight);
-
-            return vector;
-        },
-
-        _getJSON: function () {
-            return {
-                points: points,
-                t: this._t
-            };
+        if (points.length < 3) {
+            this.error("Can't sample point from SplineCurve - not enough points on curve - returning [0,0,0].");
+            return;
         }
-    });
 
-})();
+        var point = ( points.length - 1 ) * t;
+
+        var intPoint = Math.floor(point);
+        var weight = point - intPoint;
+
+        var point0 = points[intPoint === 0 ? intPoint : intPoint - 1];
+        var point1 = points[intPoint];
+        var point2 = points[intPoint > points.length - 2 ? points.length - 1 : intPoint + 1];
+        var point3 = points[intPoint > points.length - 3 ? points.length - 1 : intPoint + 2];
+
+        var vector = math.vec3();
+
+        vector[0] = math.catmullRomInterpolate(point0[0], point1[0], point2[0], point3[0], weight);
+        vector[1] = math.catmullRomInterpolate(point0[1], point1[1], point2[1], point3[1], weight);
+        vector[2] = math.catmullRomInterpolate(point0[2], point1[2], point2[2], point3[2], weight);
+
+        return vector;
+    }
+
+    getJSON() {
+        return {
+            points: points,
+            t: this._t
+        };
+    }
+};
