ffe378ed149bab029940686ae482b64a359ae3d3
xeolabs <lindsay.kay@xeolabs.com>
2018-09-04 15:56:36 +0200

    Convert Skybox to ES6

--- examples/js/skyboxes/skybox.js
+++ examples/js/skyboxes/skybox.js
@@ -78,170 +78,124 @@
  @param [cfg.active=true] {Boolean} True when this Skybox is visible.
  @extends Component
  */
-(function () {
+xeogl.Skybox = class xeoglSkyBox extends xeogl.Component {
 
-    "use strict";
+    /**
+     JavaScript class name for this Component.
 
-    xeogl.Skybox = xeogl.Component.extend({
+     For example: "xeogl.AmbientLight", "xeogl.ColorTarget", "xeogl.Lights" etc.
 
-        type: "xeogl.Skybox",
+     @property type
+     @type String
+     @final
+     */
+    get type() {
+        return "xeogl.Skybox";
+    }
 
-        _init: function (cfg) {
+    init(cfg) {
 
-            this._skyboxMesh = new xeogl.Mesh(this, {
+        super.init(cfg);
 
-                geometry: new xeogl.Geometry(this, { // Box-shaped geometry
-                    primitive: "triangles",
-                    positions: [
-                        1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, // v0-v1-v2-v3 front
-                        1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, // v0-v3-v4-v5 right
-                        1, 1, 1, 1, 1, -1, -1, 1, -1, -1, 1, 1, // v0-v5-v6-v1 top
-                        -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1, // v1-v6-v7-v2 left
-                        -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, // v7-v4-v3-v2 bottom
-                        1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1 // v4-v7-v6-v5 back
-                    ],
-                    uv: [
-                        0.5, 0.6666,
-                        0.25, 0.6666,
-                        0.25, 0.3333,
-                        0.5, 0.3333,
+        this._skyboxMesh = new xeogl.Mesh(this, {
 
-                        0.5, 0.6666,
-                        0.5, 0.3333,
-                        0.75, 0.3333,
-                        0.75, 0.6666,
-
-                        0.5, 0.6666,
-                        0.5, 1,
-                        0.25, 1,
-                        0.25, 0.6666,
-
-                        0.25, 0.6666,
-                        0.0, 0.6666,
-                        0.0, 0.3333,
-                        0.25, 0.3333,
-
-                        0.25, 0,
-                        0.50, 0,
-                        0.50, 0.3333,
-                        0.25, 0.3333,
-
-                        0.75, 0.3333,
-                        1.0, 0.3333,
-                        1.0, 0.6666,
-                        0.75, 0.6666
-                    ],
-                    indices: [
-                        0, 1, 2,
-                        0, 2, 3,
-                        4, 5, 6,
-                        4, 6, 7,
-                        8, 9, 10,
-                        8, 10, 11,
-                        12, 13, 14,
-                        12, 14, 15,
-
-                        16, 17, 18,
-                        16, 18, 19,
-
-                        20, 21, 22,
-                        20, 22, 23
-                    ]
+            geometry: new xeogl.Geometry(this, { // Box-shaped geometry
+                primitive: "triangles",
+                positions: [
+                    1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, // v0-v1-v2-v3 front
+                    1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, // v0-v3-v4-v5 right
+                    1, 1, 1, 1, 1, -1, -1, 1, -1, -1, 1, 1, // v0-v5-v6-v1 top
+                    -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1, // v1-v6-v7-v2 left
+                    -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, // v7-v4-v3-v2 bottom
+                    1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1 // v4-v7-v6-v5 back
+                ],
+                uv: [
+                    0.5, 0.6666, 0.25, 0.6666, 0.25, 0.3333, 0.5, 0.3333, 0.5, 0.6666, 0.5, 0.3333, 0.75, 0.3333, 0.75, 0.6666,
+                    0.5, 0.6666, 0.5, 1, 0.25, 1, 0.25, 0.6666, 0.25, 0.6666, 0.0, 0.6666, 0.0, 0.3333, 0.25, 0.3333,
+                    0.25, 0, 0.50, 0, 0.50, 0.3333, 0.25, 0.3333, 0.75, 0.3333, 1.0, 0.3333, 1.0, 0.6666, 0.75, 0.6666
+                ],
+                indices: [
+                    0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11,
+                    12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23
+                ]
+            }),
+            scale: [2000, 2000, 2000], // Overridden when we initialize the 'size' property, below
+            rotation: [0, -90, 0],
+            material: new xeogl.PhongMaterial(this, {
+                ambient: [0, 0, 0],
+                diffuse: [0, 0, 0],
+                specular: [0, 0, 0],
+                emissive: [1, 1, 1],
+                emissiveMap: new xeogl.Texture(this, {
+                    src: cfg.src,
+                    flipY: true,
+                    encoding: cfg.encoding || "sRGB"
                 }),
-                scale: [2000, 2000, 2000], // Overridden when we initialize the 'size' property, below
-                rotation: [0, -90, 0],
-                material: new xeogl.PhongMaterial(this, {
-                    ambient: [0, 0, 0],
-                    diffuse: [0, 0, 0],
-                    specular: [0, 0, 0],
-                    emissive: [1, 1, 1],
-                    emissiveMap: new xeogl.Texture(this, {
-                        src: cfg.src,
-                        flipY: true,
-                        encoding: cfg.encoding || "sRGB"
-                    }),
-                    backfaces: true // Show interior faces of our skybox geometry
-                }),
-                stationary: true,
-                visible: false,
-                pickable: false,
-                collidable: false
-            });
+                backfaces: true // Show interior faces of our skybox geometry
+            }),
+            stationary: true,
+            visible: false,
+            pickable: false,
+            collidable: false
+        });
 
-            this.size = cfg.size; // Sets 'xyz' property on the Mesh's Scale transform
-            this.active = cfg.active;
-        },
+        this.size = cfg.size; // Sets 'xyz' property on the Mesh's Scale transform
+        this.active = cfg.active;
+    }
 
-        _props: {
 
-            /**
-             * Size of this Skybox, given as the distance from the center at [0,0,0] to each face.
-             *
-             * Fires an {{#crossLink "Skybox/size:event"}}{{/crossLink}} event on change.
-             *
-             * @property size
-             * @default 1000
-             * @type {Number}
-             */
-            size: {
+    /**
+     * Size of this Skybox, given as the distance from the center at [0,0,0] to each face.
+     *
+     * Fires an {{#crossLink "Skybox/size:event"}}{{/crossLink}} event on change.
+     *
+     * @property size
+     * @default 1000
+     * @type {Number}
+     */
+    set size(value) {
 
-                set: function (value) {
+        this._size = value || 1000;
 
-                    this._size = value || 1000;
+        this._skyboxMesh.scale = [this._size, this._size, this._size];
 
-                    this._skyboxMesh.scale = [this._size, this._size, this._size];
+        /**
+         Fired whenever this Skybox's {{#crossLink "Skybox/size:property"}}{{/crossLink}} property changes.
 
-                    /**
-                     Fired whenever this Skybox's {{#crossLink "Skybox/size:property"}}{{/crossLink}} property changes.
+         @event size
+         @param value {Array of Number} The property's new value
+         */
+        this.fire("size", this._size);
+    }
 
-                     @event size
-                     @param value {Array of Number} The property's new value
-                     */
-                    this.fire("size", this._size);
-                },
+    get size() {
+        return this._size;
+    }
 
-                get: function () {
-                    return this._size;
-                }
-            },
 
-            /**
-             Indicates whether this Skybox is visible or not.
+    /**
+     Indicates whether this Skybox is visible or not.
 
-             Fires a {{#crossLink "Skybox/active:event"}}{{/crossLink}} event on change.
+     Fires a {{#crossLink "Skybox/active:event"}}{{/crossLink}} event on change.
 
-             @property active
-             @default false
-             @type Boolean
-             */
-            active: {
+     @property active
+     @default false
+     @type Boolean
+     */
+    set active(value) {
 
-                set: function (value) {
+        this._skyboxMesh.visible = value;
 
-                    this._skyboxMesh.visible = value;
+        /**
+         Fired whenever this Skybox's {{#crossLink "Skybox/active:property"}}{{/crossLink}} property changes.
 
-                    /**
-                     Fired whenever this Skybox's {{#crossLink "Skybox/active:property"}}{{/crossLink}} property changes.
+         @event active
+         @param value {Boolean} The property's new value
+         */
+        this.fire("active", this._skyboxMesh.visible);
+    }
 
-                     @event active
-                     @param value {Boolean} The property's new value
-                     */
-                    this.fire("active", this._skyboxMesh.visible);
-                },
-
-                get: function () {
-                    return this._skyboxMesh.visible;
-                }
-            }
-        },
-
-        _getJSON: function () {
-            return {
-                src: this._skyboxMesh.material.emissiveMap.src,
-                active: this._active,
-                size: this._size
-            };
-        }
-    });
-
-})();
+    get active() {
+        return this._skyboxMesh.visible;
+    }
+};
