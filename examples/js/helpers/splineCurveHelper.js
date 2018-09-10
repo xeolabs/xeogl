70508e62198a94e186447c4ef8f8640a3799faa2
xeolabs <lindsay.kay@xeolabs.com>
2018-09-10 23:08:29 +0200

    Converted SplineCurveHelper to EC6

--- examples/js/helpers/splineCurveHelper.js
+++ examples/js/helpers/splineCurveHelper.js
@@ -5,127 +5,73 @@
  @module meshes
  @extends Component
  */
-xeogl.SplineCurveHelper = xeogl.Component.extend({
+xeogl.SplineCurveHelper = class xeoglSplineCurveHelper extends xeogl.Component {
 
-    type: "xeogl.SplineCurveHelper",
-
-    _init: function (cfg) {
-
-        this._super(cfg);
-
+    init(cfg) {
+        super.init(cfg);
         this._divisions = 100;
 
-        this._line = new xeogl.Mesh(this.scene, {
-            geometry: new xeogl.Geometry(this.scene, {
-                primitive: "lines",
-                positions: [0, 0, 0],
-                indices: [0, 1]
-            }),
-            material: new xeogl.PhongMaterial(this.scene, {
-                diffuse: [1, 0, 0]
-            })
-        });
+        this._splineCurve = cfg.splineCurve;
+        if (this._splineCurve) {
+            const self = this;
+            this._onPathCurves = this._splineCurve.on("curves", () => {
+                self._needUpdate();
+            });
+        }
+    }
 
-        this.splineCurve = cfg.splineCurve;
-    },
-
-    _update: function () {
-
-        var splineCurve = this._children.splineCurve;
-
+    _update() {
+        if (this._line) {
+            this._line.geometry.destroy();
+            this._line.material.destroy();
+            this._line.destroy();
+        }
+        const splineCurve = this._splineCurve;
         if (!splineCurve) {
             return;
         }
-
-        var points = splineCurve.getPoints(this._divisions);
-
-        var positions = [];
-        var point;
-
+        const points = splineCurve.getPoints(this._divisions);
+        const positions = [];
+        let point;
         for (var i = 0, len = points.length; i < len; i++) {
-
             point = points[i];
-
             positions.push(point[0]);
             positions.push(point[1]);
             positions.push(point[2]);
         }
-
-        var indices = [];
-
+        const indices = [];
         for (var i = 0, len = points.length - 1; i < len; i++) {
             indices.push(i);
             indices.push(i + 1);
         }
-
-        this._line.geometry.positions = positions;
-        this._line.geometry.indices = indices;
-    },
-
-    _props: {
-
-        /**
-         * The SplineCurve for this SplineCurveHelper.
-         *
-         * Fires a {{#crossLink "SplineCurveHelper/splineCurve:event"}}{{/crossLink}} event on change.
-         *
-         * @property splinecurve
-         * @type {Splinecurve}
-         */
-        splineCurve: {
-
-            set: function (value) {
-
-                // Unsubscribe from old Curves's events
-
-                var oldSplineCurve = this._children.splineCurve;
-
-                if (oldSplineCurve && (!value || (value.id !== undefined ? value.id : value) !== oldSplinecurve.id)) {
-                    oldSplineCurve.off(this._onSplinecurveCurves);
-                }
-
-                /**
-                 * Fired whenever this CameraPaths's {{#crossLink "CameraPath/path:property"}}{{/crossLink}} property changes.
-                 * @event path
-                 * @param value The property's new value
-                 */
-                this._setChild("path", value);
-
-                var newPath = this._children.path;
-
-                if (newPath) {
-
-                    // Subscribe to new Path's curves
-
-                    var self = this;
-
-                    this._onPathCurves = newPath.on("curves",
-                        function () {
-                            self._needUpdate();
-                        });
-                }
-            },
-
-            get: function () {
-                return this._children.path;
-            }
-        }
-    },
-
-    _getJSON: function () {
-
-        var json = {
-            divisions: this._divisions
-        };
-
-        if (this._children.path) {
-            json.path = this._children.path.id;
-        }
-
-        return json;
-    },
-
-    _destroy: function () {
-        this._line.destroy();
+        this._line = new xeogl.Mesh(this, {
+            geometry: new xeogl.Geometry(this, {
+                primitive: "lines",
+                positions: positions,
+                indices: indices
+            }),
+            material: new xeogl.PhongMaterial(this, {
+                diffuse: [1, 0, 0]
+            })
+        });
     }
-});
+
+    /**
+     The SplineCurve for this SplineCurveHelper.
+
+     @property splinecurve
+     @type {SplineCurve}
+     @final
+     */
+
+    get splineCurve() {
+        return this._splineCurve;
+    }
+
+    destroy() {
+        if (this._splineCurve) {
+            this._splineCurve.off(this._onPathCurves);
+        }
+        super.destroy();
+    }
+};
