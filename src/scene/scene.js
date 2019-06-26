17ec2815c7b407200a6632792145d21c9b0500be
Markus Hoelzner <37108490+mhoelzner@users.noreply.github.com>
2019-06-26 15:24:20 +0200

    getAABB min and max comparison values
    values for comparing min and max values need to set to a maxium available value not 10000, as it is in get function of aabb

--- src/scene/scene.js
+++ src/scene/scene.js
@@ -2166,12 +2166,12 @@ class Scene extends Component {
         if (target.length === 0) {
             return this.aabb;
         }
-        let xmin = 100000;
-        let ymin = 100000;
-        let zmin = 100000;
-        let xmax = -100000;
-        let ymax = -100000;
-        let zmax = -100000;
+        let xmin = math.MAX_DOUBLE;
+        let ymin = math.MAX_DOUBLE;
+        let zmin = math.MAX_DOUBLE;
+        let xmax = -math.MAX_DOUBLE;
+        let ymax = -math.MAX_DOUBLE;
+        let zmax = -math.MAX_DOUBLE;
         let valid;
         this.withObjects(target, object => {
                 const aabb = object.aabb;
